835932ffd5c35d1d1ac2aaf432c599143c60f022
Rasul <rasulkaraev3@gmail.com>
2021-07-22 14:12:41 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const fetch = require('node-fetch')
-const nodemailer = require("nodemailer");
+const { Telegraf } = require('telegraf')
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const Pageres = require('pageres');
 
-const NEEDED_TIMES = ['19', '20', '21']
+require('dotenv').config()
+
+const NEEDED_TIMES = ['19', '18']
 
 const fetchData = async () => {
-    const response = await fetch('https://atlasbus.by/api/search?from_id=c625144&to_id=c625665&calendar_width=30&date=2021-06-18&passengers=1')
+    const response = await fetch('https://atlasbus.by/api/search?from_id=c625144&to_id=c625665&calendar_width=30&date=2021-07-23&passengers=1')
     const data = await response.json()
 
     const rides = data.rides.map(ride => {
@@ -24,42 +28,35 @@ const fetchData = async () => {
     })
 
     if (filteredRides.length > 0) {
-        sendMail(filteredRides)
+        await sendMail(filteredRides)
     }
 }
 
 async function sendMail(filteredRides) {
-    let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        auth: {
-            user: 'login', // generated ethereal user
-            pass: 'pass', // generated ethereal password
-        },
-    });
-
-    const table = `<table border="1" width="50%" cellpadding="5">
+    const table = `<table style="border: 4px double black;">
                         <tr>
-                            <th>Мест</th>
-                            <th>Время</th>
+                            <th style="border: 1px solid black;">Mest</th>
+                            <th style="border: 1px solid black;">Vremya</th>
                         </tr>
                         ${filteredRides.map(ride => {
-                            return `<tr><td>${ride.freSeats}</td><td>${ride.departure}</td></tr>`
+                            return `<tr><td style="border: 1px solid black">${ride.freSeats}</td><td style="border: 1px solid black">${ride.departure}</td></tr>`
                         }).join('')}
-                    </table>
-                    <p>
-                        <a href="https://atlasbus.by/%D0%9C%D0%B0%D1%80%D1%88%D1%80%D1%83%D1%82%D1%8B/%D0%9C%D0%B8%D0%BD%D1%81%D0%BA/%D0%9C%D0%BE%D0%B3%D0%B8%D0%BB%D1%91%D0%B2?date=2021-06-18&passengers=1">
-                            ссылка
-                        </a>
-                    </p>`
+                    </table>`
 
-    await transporter.sendMail({
-        from: 'Shitan machine', // sender address
-        to: "rasulkaraev3@gmail.com", // list of receivers
-        subject: "🔥 SHAITAN MACHINE 🔥", // Subject line
-        text: JSON.stringify(filteredRides) + '\n', // plain text body
-        html: table
-    });
+    try {
+        await new Pageres()
+            .src(`data:text/html,${table}`, ['300x300'], {filename: 'schedule'})
+            .run()
+            .then(async image => {
+                await bot.telegram.sendPhoto(140385197, {source: image[0]})
+                await bot.telegram.sendMessage(140385197,'<a href="https://atlasbus.by/%D0%9C%D0%B0%D1%80%D1%88%D1%80%D1%83%D1%82%D1%8B/%D0%9C%D0%B8%D0%BD%D1%81%D0%BA/%D0%9C%D0%BE%D0%B3%D0%B8%D0%BB%D1%91%D0%B2?date=2021-07-23&passengers=1">Ssilka</a>', {parse_mode: 'HTML'})
+            })
+            .catch(e => console.log(e))
+    } catch (e) {
+        console.log(e)
+    }
 }
 
 setInterval(fetchData, 30000)
+
+bot.launch()
