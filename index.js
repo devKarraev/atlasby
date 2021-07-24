8f11dbd9df4d25f1bfb75bcb6e62f15a000c14f2
rasulkaraev <rasulkaraev3@gmail.com>
2021-07-24 13:22:39 +0300

    Created variables for necessary fields

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ require('dotenv').config()
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const NEEDED_TIMES = ['19', '18']
+const ATLAS_DAY = process.env.ATLAS_DAY
+const ATLAS_MONTH = process.env.ATLAS_MONTH
+const CHAT_ID = 140385197
 
 const fetchData = async () => {
-    const response = await fetch('https://atlasbus.by/api/search?from_id=c625144&to_id=c625665&calendar_width=30&date=2021-07-23&passengers=1')
+    const response = await fetch(`https://atlasbus.by/api/search?from_id=c625144&to_id=c625665&calendar_width=30&date=2021-${ATLAS_MONTH}-${ATLAS_DAY}&passengers=1`)
     const data = await response.json()
 
     const rides = data.rides.map(ride => {
@@ -29,11 +32,11 @@ const fetchData = async () => {
     })
 
     if (filteredRides.length > 0) {
-        await sendMail(filteredRides)
+        await sendMessage(filteredRides)
     }
 }
 
-async function sendMail(filteredRides) {
+async function sendMessage(filteredRides) {
     const table = `<table style="border: 4px double black;">
                         <tr>
                             <th style="border: 1px solid black;">Mest</th>
@@ -48,8 +51,8 @@ async function sendMail(filteredRides) {
         .src(`data:text/html,${table}`, ['300x300'], {filename: 'schedule'})
         .run()
         .then(async image => {
-            await bot.telegram.sendPhoto(140385197, {source: image[0]})
-            await bot.telegram.sendMessage(140385197,'<a href="https://atlasbus.by/%D0%9C%D0%B0%D1%80%D1%88%D1%80%D1%83%D1%82%D1%8B/%D0%9C%D0%B8%D0%BD%D1%81%D0%BA/%D0%9C%D0%BE%D0%B3%D0%B8%D0%BB%D1%91%D0%B2?date=2021-07-23&passengers=1">Ssilka</a>', {parse_mode: 'HTML'})
+            await bot.telegram.sendPhoto(CHAT_ID, {source: image[0]})
+            await bot.telegram.sendMessage(CHAT_ID,`<a href="https://atlasbus.by/%D0%9C%D0%B0%D1%80%D1%88%D1%80%D1%83%D1%82%D1%8B/%D0%9C%D0%B8%D0%BD%D1%81%D0%BA/%D0%9C%D0%BE%D0%B3%D0%B8%D0%BB%D1%91%D0%B2?date=2021-${ATLAS_DAY}-${ATLAS_MONTH}&passengers=1">Ssilka</a>`, {parse_mode: 'HTML'})
         })
         .catch(e => console.log(e))
 }
